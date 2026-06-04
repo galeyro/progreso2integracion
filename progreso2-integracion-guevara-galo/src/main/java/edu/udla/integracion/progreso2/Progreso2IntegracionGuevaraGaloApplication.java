882498204979d1ef965b3a97e0ee04b2487f8f28
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2026-06-03 19:38:38 -0500

    chore: implementar la base del proyecto con dependencias necesarias de mvn

---
+++ progreso2-integracion-guevara-galo/src/main/java/edu/udla/integracion/progreso2/Progreso2IntegracionGuevaraGaloApplication.java
@@ -0,0 +1,13 @@
+package edu.udla.integracion.progreso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progreso2IntegracionGuevaraGaloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Progreso2IntegracionGuevaraGaloApplication.class, args);
+    }
+
+}
